34a61e5b92bdd7530677aabc2ae5a1c02d5e4d0c
umanggisme-dotcom <umanggisme@gmail.com>
2025-09-13 23:20:29 +0530

    Add files via upload

---
+++ Delete.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Delete {
+    public static void main(String[] args) {
+        List<Integer> numbers =new ArrayList<>();
+        numbers.add(5);
+        numbers.add(32);
+        numbers.add(76);
+        numbers.add(45); 
+        System.out.println(numbers);
+    }
+}
